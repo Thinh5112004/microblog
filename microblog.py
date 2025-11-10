cd1ad576f825913d13dd906f37e750ef428399ee
Thinh5112004 <quochungthinh1990@gmail.com>
2025-11-10 21:29:48 +0000

    chapter14

--- microblog.py
+++ microblog.py
@@ -2,6 +2,8 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db, cli
 from app.models import User, Post
+from translate import translate
+translate.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
